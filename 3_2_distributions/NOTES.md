0fbb22d63116fab71c6dd1a8d2afbdab24d7dbae
Will Luckman <wmluckman@gmail.com>
2022-11-15 13:36:50 -0500

    completed tutorials

--- 3_2_distributions/NOTES.md
+++ 3_2_distributions/NOTES.md
@@ -12,3 +12,15 @@ Sometimes it helps to formulate what you understood and where you got stuck in o
 If you find you're not able complete this week's assignment, reflecting on where you are getting stuck here will help you get full credit for this week's tutorial
 
 ------------
+
+For this tutorial, I'm going to build off of my first distribution tutorial. I'm going to see if I can tweak the transitions I built into that to function more cleanly, and apply them to a dropdown filter instead of upon page load. I'll also add some axes. 
+
+I'm starting by copying over and tweaking code from my completed tutorial 2_2. 
+
+This pretty much worked as planned. At least I nailed my transitions. The parts where I'm still running into trouble are:
+
+    * Customizing my axes. It feels like this should be simple, but I've really been struggling with this throughout the tutorials. I did a little better here than the last tutorial, getting my labels to display and customizing my tick text as well...
+
+    * Having the text of the dropdown match the initial state of the page. Do I need to add a function or attribute during the "option" element setup that determines this? Not sure why it doesn't get set by the int()...
+
+With more time, I would append labels (active on hover) to each dot, with information on State, Population, and Senator name. I could aslo add a bit of CSS to the dropdown to incorporate it better.
--- 3_2_distributions/main.js
+++ 3_2_distributions/main.js
@@ -1,72 +1,175 @@
 /* CONSTANTS AND GLOBALS */
-// const width = ,
-//   height = ,
-//   margin = ,
-//   radius = ;
+const width = window.innerWidth * 0.7,
+  height = window.innerHeight * 0.7,
+  margin = { top: 20, bottom: 60, left: 60, right: 40 };
 
-// these variables allow us to access anything we manipulate in init() but need access to in draw().
-// All these variables are empty before we assign something to them.
-// let svg;
-// let xScale;
-// let yScale;
-// let colorScale;
+let svg;
+let xScale;
+let yScale;
+let colorScale;
+let sizeScale;
 
 /* APPLICATION STATE */
 let state = {
   data: [],
-  selectedParty: "All" // + YOUR INITIAL FILTER SELECTION
+  selectedParty: "All" // I can't figure out how to have my 
+  // dropdown accurately reflect the initial state
 };
+console.log("state2", state);
 
 /* LOAD DATA */
-d3.json("../data/environmentRatings.json", d3.autoType).then(raw_data => {
+d3.csv("LaborData.csv", d3.autoType).then(raw_data => {
   // + SET YOUR DATA PATH
   console.log("data", raw_data);
-  // save our data to application state
   state.data = raw_data;
+  console.log("state3", state);
   init();
 });
 
-/* INITIALIZING FUNCTION */
-// this will be run *one time* when the data finishes loading in
-function init() {
-  // + SCALES
 
+/* INITIALIZING FUNCTION */
+function init() {
+
+  // + SCALES
+ // xScale  - linear,count
+ xScale = d3.scaleLinear()
+ .domain([0, 1])
+ .range([margin.left, width - margin.right])
+
+// yScale - linear,count
+yScale = d3.scaleLinear()
+ .domain([0, 1])
+ .range([height - margin.bottom, margin.top])
+
+// colorScale
+colorScale = d3.scaleOrdinal()
+ .domain(["Republican", "Democrat", "ID"])
+ .range(["red", "blue", "green"])
+
+// sizeScale - square root, count
+sizeScale = d3.scaleSqrt()
+ .domain(d3.extent(state.data, d => d.Population))
+ //.range([0, 20])
+ .range([1, 70])
 
   // + AXES
+  const xAxis = d3.axisBottom(xScale)
+    .tickFormat(d => (d * 100) + '%'); //to convert to percent from decimal ranking
+  const yAxis = d3.axisLeft(yScale)
+    .tickFormat(d => (d * 100) + '%');
 
 
   // + UI ELEMENT SETUP
+  const selectElement = d3.select("#dropdown")
+
+  selectElement  
+    .selectAll("option")
+    .data([...Array.from(new Set(state.data.map(d => d.Party))), "All"])
+    .join("option")
+    .attr("value", d => d)
+    .text(d => d)
+
+  selectElement
+    .on("change", (event) => {
+      console.log(event)
+      console.log("prev", state)
+      state.selectedParty = event.target.value;
+      console.log("post", state)
+
+      draw();
+    })
+
+  console.log(selectElement)
 
 
   // + CREATE SVG ELEMENT
-
+  svg = d3.select("#container")
+  .append("svg")
+  .attr("width", width)
+  .attr("height", height)
+  .style("background-color", "rgb(213 200 175)")
 
   // + CALL AXES
+  const xAxisGroup = svg.append("g")
+    .attr("class", 'xAxis')
+    .attr("transform", `translate(${0}, ${height - margin.bottom})`) // move to the bottom
+    .call(xAxis)
 
+  const yAxisGroup = svg.append("g")
+    .attr("class", 'yAxis')
+    .attr("transform", `translate(${margin.left}, ${0})`) // align with left margin
+    .call(yAxis)
 
+  // add labels - xAxis
+  xAxisGroup.append("text")
+    .attr("class", 'axis-title')
+    .attr("x", width / 2)
+    .attr("y", 40)
+    .attr("text-anchor", "middle")
+    .attr("fill", "#333333")
+    .attr("font-family", "Amiri")
+    .attr("font-size", "1.5em")
+    .text("Ideology Score 2020")
 
-  draw(); // calls the draw function
+  // add labels - yAxis
+  yAxisGroup.append("text")
+    .attr("class", 'axis-title')
+    .attr("x", -40)
+    .attr("y", height / 2)
+    .attr("writing-mode", "vertical-lr")
+    .attr("text-anchor", "middle")
+    .attr("fill", "#333333")
+    .attr("font-family", "Amiri")
+    .attr("font-size", "1.5em")
+    .text("AFL-CIO Lifetime Labor Rating")
+  draw(); 
 }
 
 /* DRAW FUNCTION */
-// we call this every time there is an update to the data/state
 function draw() {
 
   // + FILTER DATA BASED ON STATE
   const filteredData = state.data
-    // .filter(d => state.selectedParty === "All" || state.selectedParty === d.Party)
+    .filter(d => state.selectedParty === "All" || state.selectedParty === d.Party)
 
   const dot = svg
     .selectAll("circle")
-    .data(filteredData, d => d.BioID)
+    .data(filteredData, d => d.score_name)
     .join(
       // + HANDLE ENTER SELECTION
-      enter => enter,
+      enter => enter.append("circle")
+          .attr("r", 5)
+          .attr("class", "dot")
+          .attr("cx", d => xScale(d.ideology))
+          .attr("cy", d => yScale(d.Score))
+          .attr("fill", d => colorScale(d.Party))
+          .call(sel => sel
+            .transition()
+            .duration(2000)
+            .attr("r", d => sizeScale(d.Population))
+          ),
 
       // + HANDLE UPDATE SELECTION
-      update => update,
+      update => update
+        .call(sel => sel.transition()
+          .duration(1000)
+          .attr("r", 5) 
+          .transition()
+          .duration(1000)
+          .attr("r", d => sizeScale(d.Population))      
+      ),
 
       // + HANDLE EXIT SELECTION
       exit => exit
+        .call(sel => sel
+          .attr("opacity", 1)
+          .transition()
+          .duration(1000)
+          .attr("opacity", 0)
+          .attr("r", 5) 
+          .remove()
+          )
     );
 }
+
+
